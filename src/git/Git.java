3bda6462c8d110303751229ee7666da5350b9b0d
Islam <Islam@Eslam>
2017-03-25 10:55:11 +0200

    second git

--- src/git/Git.java
+++ src/git/Git.java
@@ -15,6 +15,9 @@ public class Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Islam");    }
+        System.out.println("Islam"); 
+    System.out.println("Islam");}
     
+    
+   
 }
